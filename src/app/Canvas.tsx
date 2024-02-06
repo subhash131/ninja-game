ee0205a1992a3c440eb43aca996b152e09b4b00b
subhash131 <subhashnayak131@gmail.com>
2024-02-07 03:28:31 +0530

    initial game setup

--- src/app/Canvas.tsx
+++ src/app/Canvas.tsx
@@ -1,18 +1,117 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useLayoutEffect } from "react";
 import { Sprite } from "./Sprite";
 
-const Canvas = () => {
-  useEffect(() => {
-    const canvas = document.querySelector("canvas");
-    const canvasContext = canvas?.getContext("2d");
+const attackDetection = ({
+  player,
+  enemy,
+}: {
+  player: Sprite;
+  enemy: Sprite;
+}) => {
+  return (
+    player.attackBox.width + player.attackBox.position.x >= enemy.position.x &&
+    player.attackBox.position.x <= enemy.position.x + enemy.width &&
+    player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
+    player.attackBox.position.y <= enemy.position.y + enemy.height
+  );
+};
 
-    if (canvasContext && canvas) {
-      canvas.width = 1024;
-      canvas.height = 576;
-      canvasContext.fillRect(0, 0, canvas?.width, canvas?.height);
-      const player = new Sprite({ x: 0, y: 0 }, canvasContext);
+const setCanvasSize = (canvas: HTMLCanvasElement) => {
+  canvas.width = 1024;
+  canvas.height = 576;
+};
+
+const Canvas = () => {
+  let player: Sprite, enemy: Sprite;
+  useLayoutEffect(() => {
+    const canvas = document.querySelector("canvas");
+    const canvasC = canvas?.getContext("2d");
+    if (canvas) setCanvasSize(canvas);
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "d":
+          player.keys.d.pressed = true;
+          player.lastKey = "d";
+          break;
+        case "a":
+          player.keys.a.pressed = true;
+          player.lastKey = "a";
+          break;
+        case "w":
+          if (player.velocity.y == 0) player.velocity.y = -10;
+          break;
+        case "ArrowRight":
+          enemy.keys.ArrowRight.pressed = true;
+          enemy.lastKey = "ArrowRight";
+          break;
+        case "ArrowLeft":
+          enemy.keys.ArrowLeft.pressed = true;
+          enemy.lastKey = "ArrowLeft";
+          break;
+        case "ArrowUp":
+          if (enemy.velocity.y == 0) enemy.velocity.y = -10;
+          break;
+        case "ArrowDown":
+          enemy.attack();
+          break;
+        case " ":
+          player.attack();
+          break;
+      }
+    });
+    window.addEventListener("keyup", (e) => {
+      switch (e.key) {
+        case "d":
+          player.keys.d.pressed = false;
+          break;
+        case "a":
+          player.keys.a.pressed = false;
+          break;
+        case "ArrowRight":
+          enemy.keys.ArrowRight.pressed = false;
+          break;
+        case "ArrowLeft":
+          enemy.keys.ArrowLeft.pressed = false;
+          break;
+      }
+    });
+
+    const animate = () => {
+      window.requestAnimationFrame(animate);
+      if (canvasC) {
+        canvasC.fillStyle = "black";
+        canvasC?.fillRect(0, 0, canvas?.width!, canvas?.height!);
+      }
+      if (attackDetection({ enemy, player }) && player.isAttacking) {
+        console.log("player attack");
+      }
+      if (attackDetection({ enemy, player }) && enemy.isAttacking) {
+        console.log("enemy attack");
+      }
+      player.update();
+      enemy.update();
+    };
+
+    if (canvas) {
+      player = new Sprite({
+        canvas,
+        position: { x: 0, y: 0 },
+        velocity: { x: 0, y: 0 },
+        offset: { x: 0, y: 0 },
+        color: "blue",
+      });
+      enemy = new Sprite({
+        canvas,
+        position: { x: 400, y: 100 },
+        velocity: { x: 0, y: 1 },
+        offset: { x: -50, y: 0 },
+        color: "red",
+      });
       player.draw();
+      enemy.draw();
+      animate();
     }
   }, []);
   return <canvas></canvas>;
--- src/app/Sprite.ts
+++ src/app/Sprite.ts
@@ -1,17 +1,111 @@
-type Position = {
+type Axis = {
   x: number;
   y: number;
 };
+type Constructor = {
+  position: Axis;
+  canvas: HTMLCanvasElement;
+  velocity: Axis;
+  color: string;
+  offset: Axis;
+};
+const GRAVITY = 0.2;
 
 export class Sprite {
-  position: Position;
-  canvasContext: CanvasRenderingContext2D;
-  constructor(position: Position, canvasContext: CanvasRenderingContext2D) {
+  keys = {
+    a: {
+      pressed: false,
+    },
+    d: {
+      pressed: false,
+    },
+    ArrowRight: {
+      pressed: false,
+    },
+    ArrowLeft: {
+      pressed: false,
+    },
+  };
+  lastKey = "";
+  position;
+  velocity;
+  canvasContext;
+  canvas;
+  height = 150;
+  width = 50;
+  attackBox;
+  color;
+  isAttacking = false;
+  constructor({
+    position,
+    canvas,
+    velocity,
+    color = "red",
+    offset,
+  }: Constructor) {
     this.position = position;
-    this.canvasContext = canvasContext;
+    this.canvas = canvas;
+    this.canvasContext = canvas.getContext("2d");
+    this.canvasContext?.fillRect(0, 0, canvas?.width, canvas?.height);
+    this.velocity = velocity;
+    this.attackBox = {
+      position: { x: this.position.x, y: this.position.y },
+      height: 50,
+      width: 100,
+      offset,
+    };
+    this.color = color;
   }
+
+  attack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100);
+  }
+
   draw() {
-    this.canvasContext.fillStyle = "red";
-    this.canvasContext.fillRect(this.position.x, this.position.y, 50, 150);
+    if (this.canvasContext) {
+      this.canvasContext.fillStyle = this.color;
+      this.canvasContext.fillRect(this.position.x, this.position.y, 50, 150);
+
+      //attack box
+      if (this.isAttacking) {
+        this.canvasContext.fillStyle = "green";
+        this.canvasContext.fillRect(
+          this.attackBox.position.x,
+          this.attackBox.position.y,
+          this.attackBox.width,
+          this.attackBox.height
+        );
+      }
+    }
+  }
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.velocity.x = 0;
+    this.velocity.x = 0;
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y;
+    //player
+    if (this.keys.a.pressed && this.lastKey === "a") {
+      this.velocity.x = -5;
+    } else if (this.keys.d.pressed && this.lastKey === "d") {
+      this.velocity.x = 5;
+    }
+    //enemy
+    if (this.keys.ArrowLeft.pressed && this.lastKey === "ArrowLeft") {
+      this.velocity.x = -5;
+    } else if (this.keys.ArrowRight.pressed && this.lastKey === "ArrowRight") {
+      this.velocity.x = 5;
+    }
+    //y-position(gravity)
+    if (this.position.y + this.height >= this.canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += GRAVITY;
+    }
   }
 }
